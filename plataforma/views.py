c30ba4b127f6c5594b22b148e153f48244722804
rafamarquesrmb <rafa.rmb@gmail.com>
2022-07-09 20:40:05 -0300

    Creating the functions for the 'Patient' page.

--- plataforma/views.py
+++ plataforma/views.py
@@ -40,3 +40,37 @@ def pacientes(request):
             messages.add_message(request, constants.ERROR,
                                  'Erro ao tentar adicionar o paciente... Tente novamente mais tarde.')
             return redirect('/pacientes/')
+
+
+@login_required(login_url='/auth/logar')
+def dados_paciente_listar(request):
+    if request.method == "GET":
+        pacientes_from_nutri = Pacientes.objects.filter(nutri=request.user)
+        context = {
+            'pacientes': pacientes_from_nutri
+        }
+        return render(request, 'dados_paciente_listar.html', context)
+
+
+@login_required(login_url='/auth/logar')
+def dados_paciente(request, id):
+    paciente = Pacientes.objects.filter(id=id).filter(nutri=request.user).first()
+    if not paciente:
+        messages.add_message(request, constants.INFO, 'Paciente não encontrado...')
+        return redirect('/dados_paciente/')
+    if request.method == "GET":
+        context = {
+            'paciente': paciente
+        }
+        return render(request, 'dados_paciente.html', context)
+
+    elif request.method == "POST":
+        peso = request.POST.get('peso')
+        altura = request.POST.get('altura')
+        gordura = request.POST.get('gordura')
+        musculo = request.POST.get('musculo')
+
+        hdl = request.POST.get('hdl')
+        ldl = request.POST.get('ldl')
+        colesterol_total = request.POST.get('colesterol_total')
+        trigliceridios = request.POST.get('trigliceridios')
