b54d0308b9e9dc627a4ee85d74ca65585a934d20
rafamarquesrmb <rafa.rmb@gmail.com>
2022-07-09 16:53:24 -0300

    Creating the 'pacientes' functions

--- plataforma/views.py
+++ plataforma/views.py
@@ -5,4 +5,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/auth/logar/')
 def pacientes(request):
-    return render(request, 'paciente.html')
+    return render(request, 'pacientes.html')
